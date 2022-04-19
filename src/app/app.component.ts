1edb53ea35c625b62927c03ef6cd59beee271c0c
Dawson Whyte <DW2@myscc.ca>
2022-04-19 09:52:02 -0400

    * Assignment 6, completed adding a Class and Messgage Service to the program, message is displayed at the bottom and selected class is always id 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ClassService} from "./services/class.service";
+import {MessageService} from "./services/message.service";
+import {Content} from "./helper-files/content-interface";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DWhyteFantasyClasses';
-  constructor() {
+  classItem?: Content;
+  constructor(private classService: ClassService, private messageService: MessageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.classService.getClassItem(2).subscribe((classItem => this.classItem = classItem));
   }
 }
